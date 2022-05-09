3b28c703ed1dbdc744f2c67274720e3db835cf18
paulayong <pyongzea@gmail.com>
2022-05-09 00:31:48 -0500

    Desafio depuración finalizada

---
+++ Yong_Paula_Depuración.js
@@ -0,0 +1,7 @@
+//Usando lo que sabes sobre las funciones, variables, console.log y ahora return, arregla el siguiente código.
+
+function greeting(){
+        return "Hola, mundo";
+    }
+    var word = greeting();
+    console.log(word);
